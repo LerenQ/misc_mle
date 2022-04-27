f55ee3d6229dba23275a180758f1a629572e6892
lerenq <qianleren@gmail.com>
2022-04-26 23:41:08 -0400

    04-26-22

--- leetcode/bit_manipulation/260_singleNumber.py
+++ leetcode/bit_manipulation/260_singleNumber.py
@@ -3,11 +3,14 @@ from functools import reduce
 class Solution:
     def singleNumber(self, nums: 'list[int]') -> int:
         '''
-        Given a non-empty array of integers nums, 
-        every element appears three times except for one. Find that single one.
+        Given an integer array nums, 
+        in which exactly two elements appear only once 
+        and all the other elements appear exactly twice. 
+        
+        Find the two elements that appear only once. 
+        You can return the answer in any order.
+        You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
 
-        You must implement a solution with a linear runtime complexity 
-        and use only constant extra space.
         
         '''       
         ans = 0
